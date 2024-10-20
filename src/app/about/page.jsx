ad7f5a402d239f29fede9d865e3aa64a08480e0c
Avishka Gihan <87961869+AvishkaGihan@users.noreply.github.com>
2024-10-20 15:08:45 +0530

    Refactor about page: Update title, description, and content for Scribble

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -6,45 +6,43 @@ export const metadata = {
   description: "About description",
 };
 
-
 const AboutPage = () => {
-
   // console.log("lets check where it works")
   return (
     <div className={styles.container}>
       <div className={styles.textContainer}>
-        <h2 className={styles.subtitle}>About Agency</h2>
+        <h2 className={styles.subtitle}>About Scribble</h2>
         <h1 className={styles.title}>
-          We create digital ideas that are bigger, bolder, braver and better.
+          Empowering developers with Next.js insights for innovative web
+          solutions
         </h1>
         <p className={styles.desc}>
-          We create digital ideas that are bigger, bolder, braver and better. We
-          believe in good ideas flexibility and precission We’re world’s Our
-          Special Team best consulting & finance solution provider. Wide range
-          of web and software development services.
+          At Scribble, we&apos;re on a mission to revolutionize web development
+          through the power of Next.js. Our blog is a hub for developers seeking
+          cutting-edge techniques, best practices, and innovative solutions. We
+          dive deep into the intricacies of Next.js, exploring its vast
+          potential to create lightning-fast, scalable, and user-centric web
+          applications. Whether you&apos;re a seasoned pro or just starting your
+          journey, Scribble is your compass in the ever-evolving landscape of
+          modern web development.
         </p>
         <div className={styles.boxes}>
           <div className={styles.box}>
-            <h1>10 K+</h1>
-            <p>Year of experience</p>
+            <h1>100+</h1>
+            <p>In-depth Next.js Tutorials</p>
           </div>
           <div className={styles.box}>
-            <h1>10 K+</h1>
-            <p>Year of experience</p>
+            <h1>1M+</h1>
+            <p>Lines of Code Analyzed</p>
           </div>
           <div className={styles.box}>
-            <h1>10 K+</h1>
-            <p>Year of experience</p>
+            <h1>50K+</h1>
+            <p> Developers Empowered</p>
           </div>
         </div>
       </div>
       <div className={styles.imgContainer}>
-        <Image
-          src="/about.png"
-          alt="About Image"
-          fill
-          className={styles.img}
-        />
+        <Image src="/about.png" alt="About Image" fill className={styles.img} />
       </div>
     </div>
   );
